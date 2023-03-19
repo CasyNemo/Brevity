e21c386b8c6483d3777e0480fc98ef9ff18b5fef
CasyNemo <126955165+CasyNemo@users.noreply.github.com>
2023-03-19 23:18:25 +1100

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, make_response
+
+app = Flask(__name__)
+
+@app.after_request
+def add_header(response):
+    response.headers['Cache-Control'] = 'no-store'
+    return response
+
+@app.route('/summarization', methods=['GET', 'POST'])
+def summarization():
+    if request.method == 'POST':
+        read_time = int(request.form.get('read_time', 1))
+        summaries = test.get_summaries(read_time)
+        return render_template('index.html', summaries=summaries)
+    return render_template('index.html')
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
