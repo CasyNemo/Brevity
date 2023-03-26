84b09383af8d2c43326f4c9f242d23e5d749cde3
CasyNemo <126955165+CasyNemo@users.noreply.github.com>
2023-03-26 19:05:15 +1100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, make_response
 from flask import render_template
 from flask import Flask, request
-from test import test
+from summarization_utils import get_summaries
 
 app = Flask(__name__)
 
